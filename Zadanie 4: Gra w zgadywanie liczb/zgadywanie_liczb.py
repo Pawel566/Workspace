7d61abe07c6718d72b49c06f2343f543ffbd2d98
Apple <apple@Apples-MacBook-Pro.local>
2023-10-31 15:50:41 +0100

    Zadanie 4 częściowo poprawione

--- Zadanie 4: Gra w zgadywanie liczb/zgadywanie_liczb.py
+++ Zadanie 4: Gra w zgadywanie liczb/zgadywanie_liczb.py
@@ -6,18 +6,27 @@ app = Flask(__name__)
 def start():
     return render_template('image.html')
 
-@app.route("/game", methods=['GET'])
+@app.route("/game", methods=['GET', 'POST'])
 def guess_the_number():
     min_number = 1
     max_number = 1000
     computer_number = ((max_number - min_number) // 2) + min_number
-    return  render_template('game.html')
+    if request.method == 'POST':
+        your_answer = request.form.get('your_answer')
+        if your_answer == 'too big':
+            computer_number = computer_number - 1
+        elif your_answer == 'too small':
+            computer_number = computer_number + 1
+        else:
+            your_answer = 'you win'
+            return (f"I won it is {computer_number}!")
 
 
-@app.route("/end", methods=['GET'])
-def computer_won():
-    messeage = f("I won! It is {computer number}")
-    return render_template('end.html', messeage=messeage)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
